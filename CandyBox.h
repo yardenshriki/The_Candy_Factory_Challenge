00184206a5f25819d0bb9e6517ad1374a682ab92
liorZahavi <lior.zahavi@stu.shenkar.ac.il>
2026-01-21 13:47:26 +0200

    fixing the CandyBox.h
    putting all the function of the template in the same page

--- CandyBox.h
+++ CandyBox.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <type_traits>
+#include "stdio.h"
+#include "cstring"
 namespace CandyFactory
 {
     template <typename T>
@@ -16,5 +19,51 @@ namespace CandyFactory
         bool removeItem(int index);
         void displayItems() const;
     };
+    template <typename T>
+    CandyBox<T>::CandyBox(int size) : items(nullptr), capacity(size), count(0)
+    {
+        if (size > 0)
+        {
+            items = new T[capacity];
+        }
+    }
+    template <typename T>
+    CandyBox<T>::~CandyBox()
+    {
+        delete[] items;
+        items = nullptr;
+        capacity = 0;
+        count = 0;
+    }
+    template <typename T>
+    bool CandyBox<T>::addItem(const T &newItem)
+    {
+        if (count >= capacity)
+            return false;
+        items[count] = newItem;
+        count++;
+        return true;
+    }
+    template <typename T>
+    bool CandyBox<T>::removeItem(int index)
+    {
+        if (0 <= index && index < count)
+        {
+            items[index] = items[count - 1];
+            count--;
+            return true;
+        }
+        return false;
+    }
+    template <typename T>
+    void CandyBox<T>::displayItems() const
+    {
+        for (int i = 0; i < count; i++)
+        {
+            if constexpr (std::is_same_v<T, char *>)
+            {
+                std::cout << items[i] << std::endl;
+            }
+        }
+    }
 }
-#include "Candy.inl"
