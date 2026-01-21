1c075aba14640eb900305abb409e933a4187e662
yardenshriki <yarden.shriki@stu.shenkar.ac.il>
2026-01-21 13:10:28 +0200

    Update SugarSupplier.cpp

--- SugarSupplier.cpp
+++ SugarSupplier.cpp
@@ -10,16 +10,23 @@ namespace CandyFactory {
         {
             supplierName = "" ;
         }
+        this->supplierName =  new char[std::strlen(supplierName)+1] ;
+        std::strcpy (this->supplierName ,supplierName);
     }
     SugarSupplier::~SugarSupplier()
     {
         delete [] supplierName;
+        supplierName = nullptr;
     }
     void SugarSupplier::setSugarName(const char* newName) // Setter
     {
         delete [] supplierName;
+        if (!newName){ newName= "" ;}
+        this->supplierName =  new char[std::strlen(newName)+1] ;
+        std::strcpy (this->supplierName , newName );
     }
     void SugarSupplier::supplySugar()
     {
+        std::cout << supplierName << "is now supplying sugar!"<< std::endl;
     }
 }
